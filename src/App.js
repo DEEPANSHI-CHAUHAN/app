616e4b56bff2d2dc80f07763a9a341ddd8e50524
Deepanshi Chauhan <deepanshiichauhann@gmail.com>
2022-10-10 09:23:02 +1000

    Implemented Axios to fetch REST API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-
+import { useEffect, useState } from "react";
+import axios from 'axios';
 function App() {
+  const [name, setName] = useState("");
+  const displayName = () => {
+    axios.get("Link to REST API").then((response) => {
+      setName(response.data.name)
+    });
+  }
+  useEffect(()=> {
+    displayName()
+  },[]);
   return (
     <div className="App">
     <h2>Press button to get results!</h2>
-    <button> Display Name</button>
+    <button > Display Name</button>
     </div>
   );
 }
